7d5f52ff6712c18192fadd44074c8803f3989a75
Rashmi Sharma <rksharma2679@gmail.com>
2020-03-30 11:12:04 -0400

    Adding server functionality and images

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgerController");
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () => {
+  // Log (server-side) when our server has started
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
